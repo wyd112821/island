fac113a2e5175b4fb1c455bcff606e70122ca6eb
Wuyadong <985979261@qq.com>
2020-06-17 00:00:13 +0800

    island-min-test

--- app/api/v1/token.js
+++ app/api/v1/token.js
@@ -4,6 +4,7 @@ const { LoginType } = require('../../lib/enum')
 const { User } = require('../../models/user')
 const { generateToken } = require('../../../core/util')
 const { Auth } = require('../../../middlewares/auth')
+const { WXManager } = require('../../services/wx')
 
 const router = new Router({
     prefix: '/v1/token'
@@ -17,6 +18,7 @@ router.post('/', async (ctx) => {
             token = await emailLogin(v.get('body.account'), v.get('body.secret'))
             break;
         case LoginType.USER_MINI_PROGRAM:
+            token = await WXManager.codeToToken(v.get('body.account'))
             break;
         case LoginType.ADMIN_EMAIL:
             break;
--- app/services/wx.js
+++ app/services/wx.js
@@ -1,5 +1,8 @@
 const util = require('util')
 const { axios } = require('axios')
+const { User } = require('../models/user')
+const { generateToken } = require('../../core/util')
+const { Auth } = require('../../middlewares/auth')
 
 class WXManager {
     static async codeToToken(code) {
@@ -7,11 +10,22 @@ class WXManager {
 
         const result = await axios.get(url)
         if(result.status !== 200) {
-            throw new global.TextEncoderStream.AuthFailed('openid获取失败')
+            throw new global.errs.AuthFailed('openid获取失败')
         }
         const errcode = result.data.errcode
         if(errcode !== 0) {
-            throw new global.TextEncoderStream.AuthFailed('openid获取失败:' + errcode)
+            throw new global.errs.AuthFailed('openid获取失败:' + errcode)
         }
+
+        let user = await User.getUserByOpenid(result.data.openid)
+        if(!user) {
+            user = await User.registerByOpenid(result.data.openid)
+        }
+
+        return generateToken(user.id, Auth.USER)
     }
+}
+
+module.exports = {
+    WXManager
 }
